f95fbc345aa920aff749d6972d35f32c7500da27
Raghavendra Immadisetty <rimmadisetty@osius.com>
2022-04-27 11:14:42 +0530

    deleting all

--- import.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ami" "packer-ami" {
-         name          = "unknown"
-}
-
-resource "aws_ebs_snapshot" "packer-snap" {
-         volume_id = "unknown"
-}
--- packer/scripts/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-
-# Uninstall Ansible and remove PPA.
-sudo apt -y remove --purge ansible
-sudo apt-add-repository --remove ppa:ansible/ansible
-
-# Apt cleanup.
-sudo apt -y autoremove
-sudo apt -y update
